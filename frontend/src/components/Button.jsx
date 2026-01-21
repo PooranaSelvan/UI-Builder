696d0aefac521690e35ec79203500b0a21fc41f2
PooranaSelvan <pooranaselvan67@gmail.com>
2026-01-21 15:01:09 +0530

    Updated Navbar UI

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Button = ({className, style, children}) => {
+const Button = ({className = '', style = {}, children}) => {
   return <button className={className} style={style}>{children}</button>
 }
 
