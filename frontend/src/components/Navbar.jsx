bb028cf741a88a64fd797bbc7e9771aa0a96e168
PooranaSelvan <pooranaselvan67@gmail.com>
2026-01-21 14:33:05 +0530

    Completed Navbar with Styles

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,22 +6,22 @@ import Button from './Button';
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <div style={{display : "flex", flexWrap : "wrap", gap : "10px"}}>
+      <div className='nav-flex'>
         <img src={sirpamLogo} alt="" className='sirpam-logo' />
-        <h1>Navbar</h1>
+        <h2 className='nav-heading'>Sirpam</h2>
       </div>
 
       <div>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+        <ul className='nav-flex navbar-navigations'>
+          <li>Feature</li>
+          <li>Docs</li>
+          <li>Workspace</li>
+          <li>Templates</li>
         </ul>
       </div>
-      <div>
-        <Button>Login</Button>
-        <Button>SignUp</Button>
+      <div className='nav-flex'>
+        <Button className='nav-btn nav-btn-primary'>Login</Button>
+        <Button className='nav-btn nav-btn-secondary'>Get Started</Button>
       </div>
     </div>
   )
