31d077fbbe062fc07067b7f216a5e2d437dce435
PooranaSelvan <pooranaselvan67@gmail.com>
2026-01-23 07:52:00 +0530

    Updated Navbar in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
-        {/* <Navbar /> */}
+        <Navbar />  
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='*' element={<ErrorPage />}  />
