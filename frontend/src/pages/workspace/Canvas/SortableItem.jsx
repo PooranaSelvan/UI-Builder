47684203f2a934ca8076c998e4fa5bbd15a3fce0
PooranaSelvan <pooranaselvan67@gmail.com>
2026-01-28 15:46:22 +0530

    Fixed Workspace

--- frontend/src/pages/workspace/Canvas/SortableItem.jsx
+++ frontend/src/pages/workspace/Canvas/SortableItem.jsx
@@ -4,10 +4,12 @@ import React from "react";
 
 const SortableItem = ({ ele }) => {
      const { id, tag, content, defaultProps, children } = ele;
-     const { setNodeRef, attributes, listeners, transform, transition, isOver } = useSortable({ id });
+     const { setNodeRef, attributes, listeners, transform, transition, isOver } = useSortable({ id }); 
+
+     console.log(listeners, defaultProps);
 
      const style = {
-          transform: CSS.Transform.toString(transform),
+          transform: transform ? `translate3d(${transform.x}px, ${transform.y}px, 0)` : undefined,
           transition,
           border: isOver ? "5px solid deepskyblue" : "none",
           marginBottom: 10
@@ -19,12 +21,12 @@ const SortableItem = ({ ele }) => {
           if (typeof tag === "string") {
                return React.createElement(tag, defaultProps, childrenElements);
           }
-          
+
           return React.createElement(tag, defaultProps);
      };
 
      return (
-          <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+          <div ref={setNodeRef} style={style} id={id} {...attributes} {...listeners}>
                {renderComponent()}
           </div>
      );
