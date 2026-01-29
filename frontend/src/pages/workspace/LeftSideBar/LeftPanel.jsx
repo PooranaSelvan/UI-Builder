570f60c949b86fb08c5d9a0e16bdc04f925116d7
lubinax2008-star <lubina.x2008@gmail.com>
2026-01-29 15:31:30 +0530

    Added Resizable In LeftPanel

--- frontend/src/pages/workspace/LeftSideBar/LeftPanel.jsx
+++ frontend/src/pages/workspace/LeftSideBar/LeftPanel.jsx
@@ -2,10 +2,36 @@ import "./LeftPanel.css";
 import { components } from "../utils/ComponentsData";
 import ComponentItem from "./ComponentItem";
 import { Search } from "lucide-react";
+import { useState,useRef } from "react";
 
 export default function LeftPanel() {
+  const [width, setWidth] = useState(280); 
+  const isResizing = useRef(false);
+
+  const startResize = () => {
+    isResizing.current = true;
+    document.body.style.userSelect = "none"; 
+    document.addEventListener("mousemove", resize);
+    document.addEventListener("mouseup", stopResize);
+  };
+
+  const resize = (e) => {
+    if (!isResizing.current){
+      return;
+    } 
+    const newWidth = Math.min(Math.max(e.clientX, 220), 300);
+    setWidth(newWidth);
+  };
+
+  const stopResize = () => {
+    isResizing.current = false;
+    document.body.style.userSelect = "auto";
+    document.removeEventListener("mousemove", resize);
+    document.removeEventListener("mouseup", stopResize);
+  };
   return (
-    <aside className="left-panel">
+    <aside style={{width}} className="left-panel">
+       <div className="resize-handle" onMouseDown={startResize} />
       <div className="panel-tabs">
         <button className="tab active">Components</button>
         <button className="tab">Layers</button>
