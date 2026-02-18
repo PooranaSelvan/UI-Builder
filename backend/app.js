99b0dec6acb28feabaaf330e023372a1365bd8ee
PooranaSelvan <pooranaselvan67@gmail.com>
2026-02-18 15:41:01 +0530

    #2 Fixing Token Issue after Hosting

--- backend/app.js
+++ backend/app.js
@@ -15,10 +15,7 @@ import con from "./db/config.js";
 const PORT = process.env.PORT || 5000;
 const app = express();
 dotenv.config();
-app.use(cors({
-     origin: [process.env.FRONTEND_LOCALURL, process.env.FRONTEND_PRODURL],
-     credentials: true
-}));
+app.use(cors({ origin: "*", credentials: true}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
